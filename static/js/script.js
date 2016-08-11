610d88b4d2d1a8be1fd3dee49cac49647843c34c
Thiago Margarida <thiago.margarida@controller.education>
2016-08-11 14:59:54 -0300

    Remove Fancybox

--- static/js/script.js
+++ static/js/script.js
@@ -58,23 +58,14 @@
   // Caption
   $('.article-entry').each(function(i){
     $(this).find('img').each(function(){
-      if ($(this).parent().hasClass('fancybox')) return;
-
       var alt = this.alt;
 
       if (alt) $(this).after('<span class="caption">' + alt + '</span>');
 
-      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
+      $(this).wrap('<a href="' + this.src + '" title="' + alt + '></a>');
     });
 
-    $(this).find('.fancybox').each(function(){
-      $(this).attr('rel', 'article' + i);
-    });
   });
-
-  if ($.fancybox){
-    $('.fancybox').fancybox();
-  }
 
   // Profile card
   $(document).on('click', function () {
